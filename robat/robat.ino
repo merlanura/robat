985a44b76e9df1c53057cc03745fc2840b2c2a8e
Michael Ibsen <michael.ibsen@xisio.com>
2019-04-12 10:29:52 +0200

    Lizenz und Header

--- robat/robat.ino
+++ robat/robat.ino
@@ -1,14 +1,31 @@
 /**
+ * Steuersoftware für den Roboterbausatz RoBat
+ * 
+ * https://github.com/merlanura/robat
+ * https://robot-workshop.de/
+ * 
+ * Copyright (C) 2018 Michael Ibsen
  *
- * RoBat - Robot Construction Kit
- *
- * Michael Ibsen
  * ibsen@gmx.net
  *
- * 2019-04-11
+ * 2019-04-12
  *
  * License: GNU GPLv3
- *
+ * http://www.gnu.de/documents/gpl.de.html
+ * 
+ * Dieses Programm ist freie Software. Sie können es unter den Bedingungen 
+ * der GNU General Public License, wie von der Free Software Foundation 
+ * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß 
+ * Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren Version.
+ * 
+ * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es 
+ * Ihnen von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne 
+ * die implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR 
+ * EINEN BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public 
+ * License.
+ * 
+ * Sie sollten ein Exemplar der GNU General Public License zusammen mit 
+ * diesem Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
  *
  */
 
